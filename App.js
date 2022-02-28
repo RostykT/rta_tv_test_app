65f19269ee8e484c10e432c6fe0970c054c0b202
Ross <rostyslav.kamyshanov@globallogic.com>
2022-02-28 23:42:22 +0200

    login screen

--- App.js
+++ App.js
@@ -1,95 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import ReactNative, {
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Button,
-} from 'react-native';
-
-import {useState, useRef} from 'react';
+import LoginScreen from './src/screens/LoginScreen';
 
 const App = () => {
-  const emailInput = useRef();
-  const passwordInput = useRef();
-  const [error, SetError] = useState(false);
-  const [email, SetEmail] = useState('');
-  const [password, SetPassword] = useState('');
-  const userAdmin = {
-    email: 'roku@roku.com',
-    password: 'roku',
-  };
-  const handleSubmit = () => {
-    if (email === userAdmin.email && password === userAdmin.password) {
-      console.log('sign in successfully');
-      //   NavigateToHome();
-      SetError(false);
-    } else {
-      SetError(true);
-      console.log('Russian warship, go fuck yourself');
-    }
-  };
-  const styles = StyleSheet.create({
-    sectionContainer: {
-      marginTop: 32,
-      paddingHorizontal: 24,
-    },
-    sectionTitle: {
-      fontSize: 24,
-      fontWeight: '600',
-    },
-    sectionDescription: {
-      marginTop: 8,
-      fontSize: 18,
-      fontWeight: '400',
-    },
-    highlight: {
-      fontWeight: '700',
-    },
-    input: {
-      width: 300,
-      borderColor: error ? 'red' : 'black',
-      borderWidth: 1,
-      marginVertical: 5,
-    },
-  });
-
   return (
-    <SafeAreaView>
-      <Text>Login</Text>
-      <TouchableOpacity onFocus={() => emailInput.current.focus()}>
-        <TextInput
-          style={styles.input}
-          ref={emailInput}
-          placeholder="Email"
-          onChangeText={email => SetEmail(email)}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onFocus={() => passwordInput.current.focus()}>
-        <TextInput
-          style={styles.input}
-          ref={passwordInput}
-          placeholder="Password"
-          onChangeText={password => SetPassword(password)}
-        />
-      </TouchableOpacity>
-      <Button title="login" onPress={handleSubmit} />
-    </SafeAreaView>
+    <View>
+      <LoginScreen />
+    </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import ReactNative, {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import {useState, useRef} from 'react';
+
+const LoginScreen = () => {
+  const emailInput = useRef();
+  const passwordInput = useRef();
+  const [error, SetError] = useState(false);
+  const [email, SetEmail] = useState('');
+  const [password, SetPassword] = useState('');
+  const userAdmin = {
+    email: 'roku@roku.com',
+    password: 'roku',
+  };
+  const handleSubmit = () => {
+    if (email === userAdmin.email && password === userAdmin.password) {
+      console.log('sign in successfully');
+      //   NavigateToHome();
+      SetError(false);
+    } else {
+      SetError(true);
+      console.log('Russian warship, go fuck yourself');
+    }
+  };
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginVertical: 50,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: '600',
+    },
+    button: {
+      marginTop: 10,
+      backgroundColor: '#1572A1',
+      width: 400,
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+    },
+    input: {
+      width: 400,
+      borderColor: error ? 'red' : '#D1D1D1',
+      borderWidth: 1,
+      marginTop: 10,
+      borderRadius: 5,
+      padding: 10,
+    },
+    errorText: {
+      color: 'red',
+      fontSize: 10,
+    },
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TouchableOpacity onFocus={() => emailInput.current.focus()}>
+        <View>
+          <TextInput
+            style={styles.input}
+            ref={emailInput}
+            placeholder="Email"
+            onChangeText={email => SetEmail(email)}
+          />
+          {error && (
+            <Text style={styles.errorText}>Incorrect password or email</Text>
+          )}
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onFocus={() => passwordInput.current.focus()}>
+        <View>
+          <TextInput
+            style={styles.input}
+            ref={passwordInput}
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={password => SetPassword(password)}
+          />
+          {error && (
+            <Text style={styles.errorText}>Incorrect password or email</Text>
+          )}
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={{color: 'white'}}>Login</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
